00eabb1663750980f1d70870142c8dad3843809f
iDubrovsky <iapopko1@mts.ru>
2021-09-24 15:42:00 +0300

    Getting Started with SwiftUI.

---
+++ memorize/memorize/ContentView.swift
@@ -0,0 +1,60 @@
+//
+//  ContentView.swift
+//  memorize
+//
+//  Created by Илья on 24.09.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 25.0)
+               .stroke(lineWidth: 3)
+            Text("Hello, Dubrovsky!")
+                .foregroundColor(.red)
+        })
+        .foregroundColor(.blue)
+        .padding(.horizontal)
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
